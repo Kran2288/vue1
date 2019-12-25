ef2b7954ea81efdd762b2131cea95fcb449bd545
Ayrat <56065447+Kran2288@users.noreply.github.com>
2019-12-25 13:34:30 +0300

    1Save

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var n = new Vue({
+	el: '#vue',
+	data: {
+		message: 'Подкинули, Закинули v-bind)',
+		name: '',
+		surname: '',
+		counter: 0,
+		students: []
+	},
+	methods: {
+		onSubmit() {
+			this.students.push({
+				'sName': this.name + ' ' + this.surname,
+			})
+		}
+	}
+})
